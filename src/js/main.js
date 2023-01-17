9e7605c30a487175cb4e265b0381294c7badde6f
Tiago Cintra <tiago.slv@hotmail.com>
2023-01-17 14:34:04 -0300

    mouse hover interactions created for creations cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,29 @@
-import '/src/css/index.css'
+import '/src/css/index.css';
 
-const hamMenu = document.querySelector(".hamburger-lines")
+const hamMenu = document.querySelector('.hamburger-lines');
 
-hamMenu.addEventListener('click', activateHamMenu)
+hamMenu.addEventListener('click', activateHamMenu);
 
 function activateHamMenu() {
-  hamMenu.classList.toggle("active")
+  hamMenu.classList.toggle('active');
 }
+
+const creationsCards = document.querySelectorAll('.creations-img');
+
+function handleActiveCard() {
+  this.classList.add('active');
+  this.children[this.children.length - 1].classList.add('active');
+}
+
+creationsCards.forEach((item) =>
+  item.addEventListener('mouseover', handleActiveCard)
+);
+
+function handleDeactivateCard() {
+  this.classList.remove('active');
+  this.children[this.children.length - 1].classList.remove('active');
+}
+
+creationsCards.forEach((item) =>
+  item.addEventListener('mouseleave', handleDeactivateCard)
+);
